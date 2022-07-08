614ee9ae6e40c83a01bf8c0c57761ab5bdec8751
pelek1987 <pawel.sioda@gmail.com>
2022-07-08 22:21:54 +0200

    add missing properties to dummyData object

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
-import { Header } from "../components/Header"
-import { Footer } from "../components/Footer"
-import { ProductDetails } from "../components/Product"
-import { Main } from "../components/Main"
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { ProductDetails } from "../components/Product";
+import { Main } from "../components/Main";
 
 const dummyData = {
+  id: 1,
+  title: "",
   description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, eius suscipit sunt iusto vitae architecto reiciendis magni maiores saepe, reprehenderit id distinctio dolorum est! Quos natus accusantium eius fuga velit.
   Excepturi facilis sit, incidunt minus tempora saepe praesentium ab ipsum repellendus, assumenda fuga natus. Natus laborum, eos nisi voluptatem quia saepe provident ipsa ullam iusto eum illo facere commodi nihil.
   Magni accusamus maxime odit doloremque sapiente labore ad delectus eum totam, earum odio nobis eveniet commodi accusantium impedit eaque ipsa maiores numquam quos dolorum ex! Itaque labore similique nulla asperiores.`,
-  thumbnailUrl: 'https://picsum.photos/200/300',
-  thumbnailAlt: 'Random image from lorem picsum',
-  rating: 4.5
-}
+  thumbnailUrl: "https://picsum.photos/200/300",
+  thumbnailAlt: "Random image from lorem picsum",
+  rating: 4.5,
+};
 
 const HomePage = () => {
   return (
@@ -21,7 +23,7 @@ const HomePage = () => {
       </Main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
