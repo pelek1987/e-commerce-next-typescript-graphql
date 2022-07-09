c04ebdfe67b3c0a3bcbb24e3d8ef15741b9eb82c
pelek1987 <pawel.sioda@gmail.com>
2022-07-09 02:24:13 +0200

    change api

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,34 +1,39 @@
-import Link from 'next/link'
-import { Rating } from './Rating'
+import Link from "next/link";
+import { Rating } from "./Rating";
 
 interface IProductDetails {
-  id: number,
-  title: string,
-  description: string,
-  thumbnailUrl: string,
-  thumbnailAlt: string,
-  rating: number
+  id: number;
+  title: string;
+  description: string;
+  longDescription: string;
+  thumbnailUrl: string;
+  thumbnailAlt: string;
+  rating: number;
 }
 
 interface ProductDetailsProps {
-  data: IProductDetails
+  data: IProductDetails;
 }
 
 export const ProductDetails = ({ data }: ProductDetailsProps) => {
   return (
     <>
       <img src={data.thumbnailUrl} alt={data.thumbnailAlt} />
-      <h2 className='p-4 text-2xl font-bold'>{data.title}</h2>
-      {data.description}
+      <h2 className="p-4 text-2xl font-bold">{data.title}</h2>
+      {/* <p>{data.description}</p> */}
+      <p>{data.longDescription}</p>
       <Rating rating={data.rating} />
     </>
-  )
-}
+  );
+};
 
-type ProductListItemType = Pick<IProductDetails, 'id' | 'title' | 'thumbnailUrl' | 'thumbnailAlt'>
+type ProductListItemType = Pick<
+  IProductDetails,
+  "id" | "title" | "thumbnailUrl" | "thumbnailAlt"
+>;
 
 interface ProductListItemProps {
-  data: ProductListItemType
+  data: ProductListItemType;
 }
 
 export const ProductListItem = ({ data }: ProductListItemProps) => {
@@ -36,8 +41,10 @@ export const ProductListItem = ({ data }: ProductListItemProps) => {
     <>
       <img src={data.thumbnailUrl} alt={data.thumbnailAlt} />
       <Link href={`/products/${data.id}`}>
-        <a><h2 className='p-4 text-2xl font-bold'>{data.title}</h2></a>
+        <a>
+          <h2 className="p-4 text-2xl font-bold">{data.title}</h2>
+        </a>
       </Link>
     </>
-  )
-}
+  );
+};
